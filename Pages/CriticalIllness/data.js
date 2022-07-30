3f4aba8067827316e30602bb2885f79356a2fd12
Jeremy <chiajeremy1@gmail.com>
2022-07-30 19:12:49 +0800

    New Project

---
+++ Pages/CriticalIllness/data.js
@@ -0,0 +1,17 @@
+export default [
+    {  "id":"1",
+        "title":"Heart",
+        "description":"Heart surgery is any surgery done on the heart muscle, valves, arteries, or the aorta and other large arteries connected to the heart. The term 'open heart surgery' means that you are connected to a heart-lung bypass machine, or bypass pump during surgery"
+    },
+    {
+       "id":"2",
+        "title":"Lungs",
+        "description":"Cosmetic surgery is a unique discipline of medicine focused on enhancing appearance through surgical and medical techniques. Cosmetic surgery can be performed on all areas of the head, neck and body. Because treated areas function properly but lack aesthetic appeal, cosmetic surgery is elective."
+    },
+    {
+       "id":"3",
+        "title":"Orthopedic",
+        "description":"Orthopedic surgery or orthopedics, is the branch of surgery concerned with conditions involving the musculoskeletal system. Orthopedic surgeons use both surgical and nonsurgical means to treat musculoskeletal trauma, spine diseases, sports injuries, degenerative diseases, infections, tumors, and congenital disorders."
+    },
+    
+]
---
+++ Pages/ElectiveSurgery/data.js
@@ -0,0 +1,17 @@
+export default [
+    {  "id":"1",
+        "title":"Cosmetic Surgery",
+        "description":"Cosmetic surgery is where a person chooses to have an operation, or invasive medical procedure, to change their physical appearance for cosmetic rather than medical reasons"
+    },
+    {
+       "id":"2",
+        "title":"Hernia Repair",
+        "description":"Open hernia repair is where an incision, or cut, is made in the groin. The hernia “sac” containing the bulging intestine is identified. The surgeon then pushes the hernia back into the abdomen and strengthens the abdominal wall with stitches or synthetic mesh."
+    },
+    {
+       "id":"3",
+        "title":"Sport-Related Surgery",
+        "description":"Sports injuries are injuries that occur when engaging in sports or exercise. Sports injuries can occur due to overtraining, lack of conditioning, and improper form or technique. Failing to warm up increases the risk of sports injuries. Bruises, strains, sprains, tears, and broken bones can result from sports injuries."
+    }
+
+]
